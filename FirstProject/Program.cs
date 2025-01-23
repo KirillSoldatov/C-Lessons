760ba6bb79f35990f2611bc12e851fdfb40df6cc
KirillSoldatov <132351241+KirtSoul@users.noreply.github.com>
2025-01-23 20:58:28 +0900

    Fixing comments

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -2,10 +2,10 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.Write("Input your name:");
+	static void Main(string[] args)
+	{
+		Console.Write("Input your name:");
 		var name = Console.ReadLine();
 		Console.WriteLine($"Your name {name}");
-    }
+	}
 }
