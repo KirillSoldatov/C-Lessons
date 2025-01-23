b68b9545b28db8164d9cf4c88497a55c649cda08
KirillSoldatov <132351241+KirtSoul@users.noreply.github.com>
2025-01-23 20:41:00 +0900

    Implementing changes into the FirstProject

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Input your name:");
+		var name = Console.ReadLine();
+		Console.WriteLine($"Your name {name}");
     }
 }
