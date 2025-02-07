289467ee9311be2609e9fdb5844def60090cfb71
Kirill Soldatov <ksoldatov19.94@gmail.com>
2025-02-08 08:40:22 +0900

    lesson material arrays-loops-strings

---
+++ ArraysLoopsAndStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ArraysLoopsAndStrings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+	
+	static ulong GetFactorial(ulong N)
+	{
+		if (N == 1)
+		{
+			return 1;
+		}
+		return N * GetFactorial(N - 1);
+	}
+	// Содержит ли число только простые делители 3 или 5 - Задача
+}
