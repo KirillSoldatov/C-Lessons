b10ea16b56ee1d159b0c23a2aa3884455dd0df12
Kirill Soldatov <ksoldatov19.94@gmail.com>
2025-03-06 19:05:28 +0900

    homework-unit-tests

--- TestsExample/SummaryRangeTests.cs
+++ TestsExample/SummaryRangeTests.cs
@@ -26,7 +26,76 @@ namespace TestsExample
             //Assert
             Assert.Equal(oneStringArray, summaryRange);
         }
+        [Fact]
+        public void Summary_Range_With_Two_Values_Should_Be_Two_Elements_And_One_Range()
+        {
+            //Arrange
+            int[] twoIntArray = new int[] { 1, 2 };
+            string[] oneRangeStringArray = new string[] { "1 -> 2" };
+            //Act
+            string[] summaryRange = Program.GetSummaryRange(twoIntArray);
+            //Assert
+            Assert.Equal(oneRangeStringArray, summaryRange);
+        }
+
+        [Fact]
+        public void Summary_Range_With_One_Value_In_Last_Range_Should_Be_Return_Ranges_And_Last_Element_Without_Range()
+        {
+            //Arrange
+            int[] lastElementArray = new int[] { 1, 2, 4 };
+            string[] rangeWithLastElementArray = new string[] { "1 -> 2", " 4"};
+            //Act
+            string[] summaryRange = Program.GetSummaryRange(lastElementArray);
+            //Assert
+            Assert.Equal(rangeWithLastElementArray, summaryRange);
+        }
+
+        [Fact]
+        public void Summary_Range_With_Several_Values_In_Last_Range_Should_Be_Return_Ranges()
+        {
+            //Arrange
+            int[] elementArray = new int[] { 1, 2, 4, 5 };
+            string[] rangeElementArray = new string[] { "1 -> 2", " 4 -> 5" };
+            //Act
+            string[] summaryRange = Program.GetSummaryRange(elementArray);
+            //Assert
+            Assert.Equal(rangeElementArray, summaryRange);
+        }
+
+        [Fact]
+        public void Shift_Zeros_With_One_Zero_At_Start_Of_Array_Should_Be_Return_Zero_At_End_Of_Array()
+        {
+            //Arrange
+            int[] elementArray = new int[] { 0, 1, 2, 3, 4 };
+            int[] zeroAtEndArray = new int[] { 1, 2, 3, 4, 0 };
+            //Act
+            int[] sortedElementArray = Program.ShiftZeros(elementArray);
+            //Assert
+            Assert.Equal(zeroAtEndArray, sortedElementArray);
+        }
         
-        
+        [Fact]
+        public void Shift_Zeros_With_One_Element_Array_Should_Be_Return_One_Element_Array()
+        {
+            //Arrange
+            int[] elementArray = new int[] { 0 };
+            int[] zeroAtEndArray = new int[] { 0 };
+            //Act
+            int[] sortedElementArray = Program.ShiftZeros(elementArray);
+            //Assert
+            Assert.Equal(zeroAtEndArray, sortedElementArray);
+        }
+
+        [Fact]
+        public void Find_All_Disappeared_Nums_Upd_With_Missing_Nums_Should_Be_Return_Missing_Nums()
+        {
+            //Arrange
+            int[] elementArray = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            int[] numsArray = new int[] { 5, 6 };
+            //Act
+            int[] missedNumsArray = Program.FindAllDisappearedNumsUpd(elementArray);
+            //Assert
+            Assert.Equal(numsArray, missedNumsArray);
+        }
     }
 }
